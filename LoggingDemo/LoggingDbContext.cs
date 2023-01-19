d95abe375120fb2d148fea3db539db75935a0f7a
Bilal Shabbir <bilalshabbirkind@gmail.com>
2023-01-20 04:05:49 +0500

    Add project files.

---
+++ LoggingDemo/LoggingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoggingDemo
+{
+    public class LoggingDbContext:DbContext
+    {
+        public LoggingDbContext(DbContextOptions<LoggingDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> MyProperty { get; set; }
+    }
+}
